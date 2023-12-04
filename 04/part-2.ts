433344ba916fd9ba55c90894b32f9b0a7a95ad12
Marco Slooten <marco.slooten@miele.com>
2023-12-04 23:06:07 +0100

    Day 4: part 2

--- 04/part-2.ts
+++ 04/part-2.ts
@@ -1,25 +1,36 @@
 const rawInput = await Bun.file("04/input.txt").text();
-
 const input = rawInput.split(/\n+/);
 
-const points = input.map((line) => {
-  const [firstPart, secondPart] = line.substring(10).split("|");
-  const winners = [...firstPart.match(/\d+/g)];
-  const scratchNrs = [...secondPart.match(/\d+/g)];
+const mapOfCards: { [key: string]: { value?: number; count: number } } = {};
 
-  const result = winners.map((winner) => {
-    return scratchNrs.includes(winner);
-  });
-  const nrOfCorrect = result.filter((winOrNot) => Boolean(winOrNot)).length;
-  return nrOfCorrect;
+input.forEach((line, index) => {
+  const [firstPart, secondPart] = line.substring(10).split("|");
+  const winners = firstPart.match(/\d+/g);
+  const scratchNrs = secondPart.match(/\d+/g);
+
+  const amountCorrect = winners.flatMap((winner) => {
+    return scratchNrs.includes(winner) || [];
+  }).length;
+
+  const nrOfCards = mapOfCards[index]?.count ? mapOfCards[index].count + 1 : 1;
+
+  mapOfCards[index] = {
+    value: amountCorrect,
+    count: nrOfCards,
+  };
+
+  for (let i = index + 1; i <= index + amountCorrect; i++) {
+    if (mapOfCards[i]?.count) {
+      mapOfCards[i].count = mapOfCards[i].count + nrOfCards;
+    } else {
+      mapOfCards[i] = { count: nrOfCards };
+    }
+  }
+  return amountCorrect;
 });
 
-console.log(points);
+const result = Object.values(mapOfCards).reduce((a, b) => {
+  return a + b.count;
+}, 0);
 
-// card map
-// index + 1 = card nr
-// value = number of resulting cards
-
-// card 0 => 1, 2, 3, 4, 5, 6, 7, 8
-// card 1 => 2, 3, 4, 5,
-// card 2 => 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+console.log(result);
